0dac11daca3ff3fa77a4ad80f404c5ce59994948
Benjamin2her <82844373+Benjamin2her@users.noreply.github.com>
2024-03-22 22:23:19 -0700

    Create contador_binario.ino
    Se programó el ejemplo de contador binario de 4 bits

---
+++ contador_binario.ino
@@ -0,0 +1,52 @@
+// Definición de pines para conectar LEDs
+const int ledPins[] = {2, 3, 4, 5}; // Pines para conectar los LEDs
+const int numPins = 4; // Número de LEDs
+const int limite  = pow(2, numPins);
+
+void setup() {
+  // Configuración de los pines de los LEDs
+  for (int i = 0; i < numPins; i++) {
+    pinMode(ledPins[i], OUTPUT); // Configurar los pines como salida
+  }
+}
+
+void ascendente(){
+static int contador = 0; // Variable para almacenar el valor del contador
+  
+  // Convertir el contador a binario y mostrarlo en los LEDs
+  for (int i = 0; i < numPins; i++) {
+    digitalWrite(ledPins[i], (contador >> i) & 1); // Configurar los pines de acuerdo al bit correspondiente del contador
+  }
+  
+  delay(1000); // Esperar un segundo
+  
+  // Incrementar el contador y volver a 0 si alcanza 16 (0000 a 1111)
+  contador++;
+  if (contador == limite) {
+    contador = 0;
+  }
+}
+
+void descendente(){
+static int contador = limite - 1; // Variable para almacenar el valor del contador
+  
+  // Convertir el contador a binario y mostrarlo en los LEDs
+  for (int i = 0; i < numPins; i++) {
+  // Configurar los pines de acuerdo al bit correspondiente del contador
+    digitalWrite(ledPins[i], (contador >> i) & 1); 
+  }
+  
+  delay(1000); // Esperar un segundo
+  
+  // Incrementar el contador y volver a 0 si alcanza 16 (0000 a 1111)
+  contador--;
+  if (contador == 0) {
+    contador = limite;
+  }
+}
+
+void loop() {
+  ascendente();
+  //descendente();
+  
+}
